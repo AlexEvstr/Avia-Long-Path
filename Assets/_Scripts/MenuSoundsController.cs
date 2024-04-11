dd1f599c011cb2f271c5a6e175b42ca10d355827
Alexander Evstratov <alexdrugev@me.com>
2024-04-11 12:34:07 +0300

    Add clickButton sound.

---
+++ Assets/_Scripts/MenuSoundsController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSoundsController : MonoBehaviour
+{
+    [SerializeField] private AudioClip _clickSound;
+    [SerializeField] private AudioClip _skinBuySound;
+
+    private AudioSource _audioSource;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayClickSound()
+    {
+        _audioSource.PlayOneShot(_clickSound);
+    }
+}
